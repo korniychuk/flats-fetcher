d840ed92d33d14f50c9a4c0215966ec011518461
Anton Korniychuk <dev@korniychuk.pro>
2020-11-27 21:10:33 +0200

    Text Engine complex parsing bugs fixed

--- src/services/text-engine.service.ts
+++ src/services/text-engine.service.ts
@@ -11,7 +11,7 @@ export class TextEngineService {
     }
 
     public retrieveComplex(str: string): string | undefined {
-        const regex = /(?<complex>Ж(?:ил[а-я]*)?\s*К(?:омплекс[а-я]*)|Клубн[а-я]*\s+дом[а-я]*)\s*(?<name>(?:(?:(['"]).+?\3)|(?:«.+?»)|(?:“.+?”)|[a-zа-яі'"«»]+\d*(?:(?! [св])\s+(?!(ул|вул|\d+-))[a-zа-яі\d'"«»]+){0,4}))/ig;
+        const regex = /(?<complex>Ж(?:ил[а-я]*)?\s*К(?:омплекс[а-я]*)?|Клубн[а-я]*\s+дом[а-я]*)\s*(?<name>(?:(?:(['"]).+?\3)|(?:«.+?»)|(?:“.+?”)|[a-zа-яі'"«»]+\d*(?:(?! [св])\s+(?!(ул|вул|\d+-))[a-zа-яі\d'"«»]+){0,4}))/ig;
         const values = [];
         let res;
         while (res = regex.exec(str)) {
